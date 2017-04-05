09fcb9ba9f261c6441af8e63cd8ee1cdc65beba5
godwin <godwinamila@gmail.com>
2017-04-05 16:52:38 +0530

    Change StringUtils package

--- components/org.wso2.carbon.identity.agent.userstore/src/main/java/org/wso2/carbon/identity/agent/userstore/manager/ldap/LDAPConnectionContext.java
+++ components/org.wso2.carbon.identity.agent.userstore/src/main/java/org/wso2/carbon/identity/agent/userstore/manager/ldap/LDAPConnectionContext.java
@@ -16,7 +16,7 @@
 
 package org.wso2.carbon.identity.agent.userstore.manager.ldap;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.agent.userstore.constant.LDAPConstants;

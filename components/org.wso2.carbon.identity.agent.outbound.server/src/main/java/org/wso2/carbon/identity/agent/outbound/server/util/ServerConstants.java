b3219f6bf5eea0c5cdfb2b0c4cc358851b623c81
godwin <godwinamila@gmail.com>
2017-04-19 13:13:36 +0530

    Change status constants

--- components/org.wso2.carbon.identity.agent.outbound.server/src/main/java/org/wso2/carbon/identity/agent/outbound/server/util/ServerConstants.java
+++ components/org.wso2.carbon.identity.agent.outbound.server/src/main/java/org/wso2/carbon/identity/agent/outbound/server/util/ServerConstants.java
@@ -5,6 +5,9 @@ package org.wso2.carbon.identity.agent.outbound.server.util;
  */
 public class ServerConstants {
 
-    public static final String CLIENT_CONNECTION_STATUS_ACTIVE = "A";
-    public static final String CLIENT_CONNECTION_STATUS_CLOSED = "C";
+    public static final String CLIENT_CONNECTION_STATUS_CONNECTED = "C";
+    public static final String CLIENT_CONNECTION_STATUS_CONNECTION_FAILED = "F";
+
+    public static final String ACCESS_TOKEN_STATUS_ACTIVE = "A";
+    public static final String ACCESS_TOKEN_STATUS_EXPIRED = "E";
 }
